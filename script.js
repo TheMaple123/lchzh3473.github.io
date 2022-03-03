e8fb5af2a922511736cf971099813d15e0f2e44a
d0j1a_1701 <luoyoujia666@163.com>
2022-03-03 22:12:12 +0800

    test

--- script.js
+++ script.js
@@ -168,7 +168,7 @@ const full = {
 			// if (document.mozCancelFullScreen) return document.mozCancelFullScreen();
 			// if (document.msExitFullscreen) return document.msExitFullscreen();
 			elem.pseudoFullScreen = false;
-			elem.style.margin = "auto";
+			elem.style.marginLeft = null;
 		} else {
 			if (!(elem instanceof HTMLElement)) elem = document.body;
 			// if (elem.requestFullscreen) return elem.requestFullscreen();
@@ -176,7 +176,7 @@ const full = {
 			// if (elem.mozRequestFullScreen) return elem.mozRequestFullScreen();
 			// if (elem.msRequestFullscreen) return elem.msRequestFullscreen();
 			elem.pseudoFullScreen = true;
-			elem.style.margin = "none";
+			elem.style.marginLeft = "0px";
 			resizeCanvas();
 		}
 	},
