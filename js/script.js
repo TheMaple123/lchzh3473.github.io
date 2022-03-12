915136f4e46d12c6ffc87a5070868ee0358dfc68
d0j1a_1701 <luoyoujia666@163.com>
2022-03-12 13:45:36 +0800

    fix

--- js/script.js
+++ js/script.js
@@ -12,8 +12,8 @@ function loadJS(src) {
 (async () => {
     await loadJS("/js/config.js");
     await loadJS("/js/page.js");
+    await loadJS("/js/load.js");
+    await loadJS("/js/input.js");
     await loadJS("/js/simulator.js");
-    loadJS("/js/load.js");
-    loadJS("/js/input.js");
-    loadJS("/js/render.js");
+    await loadJS("/js/render.js");
 })();
