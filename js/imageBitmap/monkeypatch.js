d04a30567fdf376ba6fb9345b9e538f0d5e4115a
d0j1a_1701 <luoyoujia666@163.com>
2022-03-12 16:47:16 +0800

    update

--- js/imageBitmap/monkeypatch.js
+++ js/imageBitmap/monkeypatch.js
@@ -13,47 +13,47 @@ import { cropImage, cropBlobImage, cropImageData } from "./image-cropper.js";
 import { convertToImageBitmap, ImageBitmapPolyfill } from "./ImageBitmap-wrapper.js";
 import { isDetachedBuffer, buildSourceIs } from "./misc.js";
 
-if( !nativeCreateImageBitmap ) {
+if (!nativeCreateImageBitmap) {
   globalThis.ImageBitmap = ImageBitmapPolyfill;
 }
 
 // https://html.spec.whatwg.org/multipage/imagebitmap-and-animations.html#dom-createimagebitmap
-globalThis.createImageBitmap = async function( source, ...args ) { // length 1
+globalThis.createImageBitmap = async function (source, ...args) { // length 1
 
-  const options = parseOptions( ...arguments );
+  const options = parseOptions(...arguments);
 
-  // with some sources (canvas & video) we need to stay synchronous until
-  // the source's buffer gets painted
-  // but some tests are async (e.g SVGImageElement)
-  // so we only await for these tests when we are sure they are required
-  const missing_features = requiresAsyncTests( source, options ) ?
-    await async_tests : tests_results;
+  // // with some sources (canvas & video) we need to stay synchronous until
+  // // the source's buffer gets painted
+  // // but some tests are async (e.g SVGImageElement)
+  // // so we only await for these tests when we are sure they are required
+  // const missing_features = requiresAsyncTests( source, options ) ?
+  //   await async_tests : tests_results;
 
-  // full compat
-  if( missing_features.size === 0 ) {
-    return nativeCreateImageBitmap( ...arguments );
-  }
+  // // full compat
+  // if( missing_features.size === 0 ) {
+  //   return nativeCreateImageBitmap( ...arguments );
+  // }
 
-  // polyfill is not required, but some features may not be supported
-  if(
-    nativeCreateImageBitmap &&
-    !requiresPolyfill( missing_features, options, source )
-  ) {
-    return nativeCreateImageBitmap( ...cleanArguments( missing_features, ...arguments ) );
-  }
+  // // polyfill is not required, but some features may not be supported
+  // if(
+  //   nativeCreateImageBitmap &&
+  //   !requiresPolyfill( missing_features, options, source )
+  // ) {
+  //   return nativeCreateImageBitmap( ...cleanArguments( missing_features, ...arguments ) );
+  // }
 
-  const sourceIs = buildSourceIs( source );
+  const sourceIs = buildSourceIs(source);
 
   let cropped_image;
-  if( canvas_sources.some( sourceIs ) ) {
-    
-    const is_usable = checkUsability( source );
+  if (canvas_sources.some(sourceIs)) {
+
+    const is_usable = checkUsability(source);
 
     // checkUsability failed to determine if source is usable,
     // as of this writing, this only concerns SVGImageElement
     // and since we do convert SVGImageElements anyway right after
     // we don't have much to do now
-    if( is_usable === "maybe" ) {
+    if (is_usable === "maybe") {
       // might want to do something someday.
     }
 
@@ -62,39 +62,39 @@ globalThis.createImageBitmap = async function( source, ...args ) { // length 1
     // so we convert these to HTMLImageElement
     // must be called after 'checkUsability' for we correctly throw with unusable media
     // (that is, for UAs that do support SVGImageElement as CanvasSource)
-    if( sourceIs( "SVGImageElement" ) ) {
-      source = await loadSVGImage( source );
+    if (sourceIs("SVGImageElement")) {
+      source = await loadSVGImage(source);
     }
-    
-    if( sourceIs( "HTMLImageElement" ) ) {
+
+    if (sourceIs("HTMLImageElement")) {
       // Since we can't detect true "image has no dimensions" (see test-support.js)
       // we unfortunately can't fall here...
       // so while per specs this should work, it's just dead code...
-      if(
+      if (
         (!source.naturalWidth && !source.naturalHeight) &&
         (!options.resizeWidth || !options.resizeHeight)
       ) {
         invalidStateImageError()
       }
-      
+
     }
-    cropped_image = cropImage( source, options );
+    cropped_image = cropImage(source, options);
 
   }
-  else if( sourceIs( "Blob" ) ) {
-    cropped_image = await cropBlobImage( source, options );
+  else if (sourceIs("Blob")) {
+    cropped_image = await cropBlobImage(source, options);
   }
-  else if( sourceIs( "ImageData" ) ) {
-    if( isDetachedBuffer( source.data.buffer ) ) {
+  else if (sourceIs("ImageData")) {
+    if (isDetachedBuffer(source.data.buffer)) {
       invalidStateImageError();
     }
-    cropped_image = await cropImageData( source, options );
+    cropped_image = await cropImageData(source, options);
   }
-  if( !cropped_image ) {
-    throw new TypeError( error_messages.INVALID_SOURCE );
+  if (!cropped_image) {
+    throw new TypeError(error_messages.INVALID_SOURCE);
   }
 
-  return await convertToImageBitmap( cropped_image );
+  return await convertToImageBitmap(cropped_image);
 
 };
 
