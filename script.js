2f79c0a417108b8b7de2162c3f4ee5d482556a7c
d0j1a_1701 <luoyoujia666@163.com>
2022-03-12 10:14:59 +0800

    upd2

--- script.js
+++ script.js
@@ -1803,13 +1803,13 @@ function range(num) {
 }
 //绘制Note
 function drawNote(note, realTime, type) {
-	//如果没有这个console.log iPhone上就无法显示Note 十分的玄学
-	if (renderFix.checked)
-		console.log("DrawNote"), message.sendMessage("DrawNote " + realTime);
 	const HL = note.isMulti && document.getElementById("highLight").checked;
 	if (!note.visible) return;
 	if (note.type != 3 && note.scored && !note.badtime) return;
 	if (note.type == 3 && note.realTime + note.realHoldTime < realTime) return; //qwq
+	//如果没有这个console.log iPhone上就无法显示Note 十分的玄学
+	if (renderFix.checked)
+		console.log("DrawNote"), message.sendMessage("DrawNote " + realTime);
 	ctxos.globalAlpha = note.alpha;
 	ctxos.setTransform(...imgFlip(noteScale * note.cosr, noteScale * note.sinr, -noteScale * note.sinr, noteScale * note.cosr, note.offsetX, note.offsetY));
 	if (type == 3) {
